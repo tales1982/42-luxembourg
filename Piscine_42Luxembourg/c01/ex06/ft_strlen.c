6348c3d12a37701619f6a3c7cda032eb568f48fb
tales1982 <102876696+tales1982@users.noreply.github.com>
2023-12-25 17:14:58 +0100

    ft_strlen

---
+++ Piscine_42Luxembourg/c01/ex06/ft_strlen.c
@@ -0,0 +1,29 @@
+/*
+Funções permitidas: Nenhuma
+• Crie uma função que conte e retorne o número de caracteres em uma string.
+• Veja como deve ser prototipado:
+int ft_strlen(char*str);
+*/
+
+#include <stdio.h>
+
+int ft_strlen(char*str);
+
+int main()
+{
+    char n[] = "Ola mundo!";//10
+    printf("Tamanho da string..: %d\n",ft_strlen(n));
+    return(0);
+}
+
+int ft_strlen(char*str)
+{
+    int i;
+
+    i = 0;
+    while(str [i] != '\0')
+    {
+        i++;
+    }
+    return(i);
+}
