98026c1333d0af4113931cfbf34ea46f0e2bbc1a
tales1982 <102876696+tales1982@users.noreply.github.com>
2023-12-30 22:31:29 +0100

    ft_strstr

--- Piscine_42Luxembourg/C03/ex04/ft_strstr.c
+++ Piscine_42Luxembourg/C03/ex04/ft_strstr.c
@@ -21,30 +21,42 @@ substring na string maior ou retorna NULL se a substring não for encontrada.
 
 char	*ft_strstr(char *str, char *to_find)
 {
-	int	i;
-	int	j;
+    int i;
+    int j;
 
-	j = 0;
-	i = 0;
-	if (to_find[j] == '\0')
-		return (str);
-	while (str[i] != '\0')
-	{
-		while (str[i + j] == to_find[j] && str[i + j] != '\0')
-			j++;
-		if (to_find[j] == '\0')
-			return (str + i);
-		i++;
-		j = 0;
-	}
-	return (0);
+    j = 0; // Inicializa o índice para a substring a procurar
+    i = 0; // Inicializa o índice para percorrer a string principal
+
+    // Se a substring a procurar é vazia, retorna a string original
+    if (to_find[j] == '\0')
+        return (str);
+
+    // Enquanto não chegarmos ao fim da string principal
+    while (str[i] != '\0')
+    {
+        // Enquanto os caracteres coincidirem e não chegarmos ao fim da substring
+        while (str[i + j] == to_find[j] && str[i + j] != '\0')
+            j++; // Avança nos índices para comparar os próximos caracteres
+
+        // Se chegamos ao fim da substring, retorna a posição atual da string principal
+        if (to_find[j] == '\0')
+            return (str + i);
+
+        // Se não, avança para o próximo caractere na string principal
+        i++;
+        j = 0; // Reinicia o índice da substring para a próxima iteração
+    }
+
+    // Se a substring não foi encontrada, retorna NULL
+    return (0);
 }
+
 
 #include<stdio.h>
 int	main(void)
 {
 	char *str = "hoje vou joga futbol no campo";
-	char *find = "futbol";
+	char *find = "no";
 
 	printf("%s\n", ft_strstr(str, find));
 	return (0);
